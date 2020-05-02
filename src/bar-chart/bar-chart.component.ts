6b357fb2856922c94982c5dd41d0606c56ad6c24
intkiran <ikiranbabu@gmail.com>
2020-05-02 10:21:19 +0530

    3 files modified

--- src/bar-chart/bar-chart.component.ts
+++ src/bar-chart/bar-chart.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bar-chart',
-  templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.css']
+  selector: "app-bar-chart",
+  templateUrl: "./bar-chart.component.html",
+  styleUrls: ["./bar-chart.component.css"]
 })
 export class BarChartComponent implements OnInit {
+  dataSource: any;
+  options1: any;
+  constructor() {
+    this.options1 = {
+      title: {
+        display: true,
+        text: "% Utilization",
+        position: "left"
+      },
+      scales: {
+        yAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              show: true,
+              labelString: "Value"
+            },
+            ticks: {
+              beginAtZero: true,
+              max: 100,
+              min: 0
+            }
+          }
+        ],
+        xAxes: [
+          {
+            categoryPercentage: 1,
+            barPercentage: 1,
+            ticks: {
+              display: false,
+              beginAtZero: 0
+            }
+          }
+        ]
+      }
+    };
 
-  constructor() { }
-
-  ngOnInit() {
+    this.dataSource = {
+      labels: ["2015", "2016", "2017", "2018", "2019", "2020"],
+      datasets: [
+        {
+          label: "Company1",
+          backgroundColor: "blue",
+          data: [25, 30, 60, 50, 80, 90]
+        },
+        {
+          label: "Company2",
+          backgroundColor: "green",
+          data: [45, 33, 70, 72, 95]
+        }
+      ]
+    };
   }
 
+  ngOnInit() {}
 }
